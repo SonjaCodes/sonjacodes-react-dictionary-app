0bf28fe34a4a9f6fa8d485502b34bcfc0ce8aaf8
SonjaCodes <sonja@kolmer.cc>
2024-09-28 16:59:51 +0200

    installed bootstrap npm + layout of App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src="SonjaCodesLogo.png"
-          className="SonjaCodesLogo"
-          alt="SonjaCodesLogo"
-        />
-      </header>
-      <body>
-        <h1>SonjaCodes React Dictionary App</h1>
-      </body>
+      <div className="container">
+        <Container className="App-Container">
+          <header className="App-header">
+            <img
+              src="SonjaCodesLogo.png"
+              className="SonjaCodesLogo img-fluid"
+              alt="SonjaCodesLogo"
+            />
+          </header>
+          <body>
+            <h1>SonjaCodes React Dictionary App</h1>
+          </body>
+        </Container>
+        <footer className="Footer">
+          This project was coded with love by{" "}
+          <a
+            href="https://github.com/SonjaCodes"
+            target="_blank"
+            rel="noreferrer">
+            Sonja Kolmer
+          </a>
+          . It is open-sourced on{" "}
+          <a
+            href="https://github.com/SonjaCodes/sonjacodes-react-dictionary-app"
+            target="_blank"
+            rel="noreferrer">
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://app.netlify.com/sites/sonjacodes-react-dictionary-app/overview"
+            target="_blank"
+            rel="noreferrer">
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
