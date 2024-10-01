f4de252f4338d88721394aeb2da40934e0eb8a51
SonjaCodes <sonja@kolmer.cc>
2024-10-01 17:01:30 +0200

    changed profile link in footer to shecodes graduate page

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,10 @@ export default function Footer() {
   return (
     <footer className="Footer">
       This project was coded with love by{" "}
-      <a href="https://github.com/SonjaCodes" target="_blank" rel="noreferrer">
+      <a
+        href="https://www.shecodes.io/graduates/123085-sonja-kolmer"
+        target="_blank"
+        rel="noreferrer">
         Sonja Kolmer
       </a>
       . It is open-sourced on{" "}
