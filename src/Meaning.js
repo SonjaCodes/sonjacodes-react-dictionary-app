865e8abf2ae5b8eb54060b9121a399804294c5a5
SonjaCodes <sonja@kolmer.cc>
2024-10-03 21:33:28 +0200

    added further styling to results

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,19 +1,21 @@
+import "./Results.css";
+
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h4>
         <i>{props.meaning.partOfSpeech}</i>
       </h4>
-      <p>
-        <u>Definition</u>: {props.meaning.definition}
-      </p>
+      <p>{props.meaning.definition}</p>
       {props.meaning.example && (
-        <p>
-          <u>Example</u>: "{props.meaning.example}"
+        <p className="example">
+          <i>
+            <u>Example</u>: "{props.meaning.example}"
+          </i>
         </p>
       )}
       {props.meaning.synonyms && (
-        <p>
+        <p className="synonyms">
           <u>Synonyms</u>: {props.meaning.synonyms?.join(", ")}
         </p>
       )}
